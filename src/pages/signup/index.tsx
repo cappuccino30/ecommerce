5ef324cf0f6ba151ab5eb6f6c16ecff873002d3c
CamilaMendes5 <camila.mendes@cadmus.com.br>
2020-11-29 03:51:18 -0300

    first commit

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Layout = ({ children }: any) => {
+return (<>
+<h1>teste</h1>
+</>);
+}
+
+export default Layout;
---
+++ src/pages/signup/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+const Title = styled.h1`
+
+`;
